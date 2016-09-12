881b6a260a98ff1a130931c91d1c6d42173d04df
Chris Churas <churas@gmail.com>
2016-09-12 16:41:45 -0700

    added load of modules.sh and unload mpi/mpich-x86_64 before loading mpi/openmpi-x86_64 which has the python mpi4py.MPI library needed by txbr

--- singularity/runscript.sh
+++ singularity/runscript.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+. /etc/profile.d/modules.sh
+module unload mpi/mpich-x86_64
 module load mpi/openmpi-x86_64
+
 export LD_LIBRARY_PATH=/lib64:/usr/lib64/mpich/lib:/usr/local/IMOD/lib:/usr/local/IMOD/qtlib
+
 . /usr/local/IMOD/IMOD-linux.sh
 
 exec /usr/bin/runtxbr.py "$@"
